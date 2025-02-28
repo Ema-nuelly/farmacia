7cf82ed87c2383515c4f4a151095332548af46a6
Ema-nuelly <emanuelly.cassia@aluno.ifsertao-pe.edu.br>
2025-02-27 23:05:31 -0300

    remover arquivos de cadastro e cabeçalho

--- conexao.php
+++ conexao.php
@@ -4,13 +4,11 @@ $username = "root";
 $password = "";
 $dbname = "farmacia";
 
-$conexao = new mysqli($servername, $username, $password, $dbname);
+// criando a conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conexao->connect_error) {
-  die("Conexão falhou: " . $conexao->connect_error);
-} else {
-  echo "<p style='display: none;'>Conexão realizada com sucesso!</p>";
+// verificando a conexão
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
 }
 ?>
-
-
